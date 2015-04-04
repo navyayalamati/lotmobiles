5a95af1367b25355935804a4e8ca3b819bf0a689
navyayalamati <navya@ostryalabs.com>
2015-04-04 08:35:17 +0530

    rename

--- rails_projects/lot_mobiles/config/application.rb
+++ rails_projects/lot_mobiles/config/application.rb
@@ -6,10 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
-  class Application < Rails::Application
+module LotMobiles
+  class LotMobileslication < Rails::LotMobileslication
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
+    # LotMobileslication configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
